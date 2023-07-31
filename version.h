aaa190e47cb21fd36ec1de098db235574a434ae8
Francesco <blushifrancesc@gmail.com>
2023-07-31 17:21:02 +0200

    Aggiunto codice versione programma

---
+++ version.h
@@ -0,0 +1,11 @@
+//
+// Created by franc on 31/07/2023.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+#include <string>
+
+static std::string VERSION = "V1.0"
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
